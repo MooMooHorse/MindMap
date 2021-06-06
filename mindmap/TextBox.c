2d268e1f68b9bf070e0b4ab217195990e0bbac14
MooMooHorse <haor.20@intl.zju.edu.cn>
2021-06-06 16:38:48 +0800

    Update TextBox.c

--- mindmap/TextBox.c
+++ mindmap/TextBox.c
@@ -7,23 +7,16 @@
 #endif
 
 
-static Textbox PositionOfTextBox[100]; 
+static Textbox PositionOfTextBox[100];
+
+static int FatherOfTextBox[100];
 
 static int TextBoxIndex;
 
 static int ActivatedTextBox;// 编号从 1 开始 = 0 表明未选中
 
 bool INBOX(double x,double y,int k){
-#if defined(DEBUG)
-	//printf("%lf %lf\n",(PositionOfTextBox[k].x+PositionOfTextBox[k].w),(PositionOfTextBox[k].y+PositionOfTextBox[k].h));
-	//printf("%d\n",x >= PositionOfTextBox[k].x
-	//	&& x <= (PositionOfTextBox[k].x+PositionOfTextBox[k].w)
-	//	&& y >= PositionOfTextBox[k].y
-	//	&& y <= (PositionOfTextBox[k].y+PositionOfTextBox[k].h)
-	//	);
-	//printf("%lf %lf\n",x,y);
-#endif
-	y=winheight-y;//为什么两个库用不同坐标系？？？？？？？ 这个GUI写的什么东西 
+	y=winheight-y;
 	return x >= PositionOfTextBox[k].x
 		&& x <= (PositionOfTextBox[k].x+PositionOfTextBox[k].w)
 		&& y >= PositionOfTextBox[k].y
@@ -34,16 +27,9 @@ bool INBOX(double x,double y,int k){
 
 void TextBoxGetMouse(int x,int y,int button,int event){
 	int i;
-#if defined(DEBUG)
-//	printf("In the TextBoxGetMouse Function\n");
-//	printf("%d\n",TextBoxIndex);
-#endif
 	for(i=1;i<=TextBoxIndex;i++){
 		if(INBOX(ScaleXInches(x),ScaleXInches(y),i)&&event==BUTTON_DOWN) {
 			ActivatedTextBox=i;
-#if defined(DEBUG)
-//			printf("Activated!\n");
-#endif
 			break;
 		}
 	}
@@ -56,13 +42,6 @@ void AddTextBox(double x,double y,double w,double h){
 	PositionOfTextBox[TextBoxIndex].y=y;
 	PositionOfTextBox[TextBoxIndex].w=w;
 	PositionOfTextBox[TextBoxIndex].h=h;
-#if defined(DEBUG)
-//	printf("%lf %lf\n",x,y);
-#endif
-
-#if defined(DEBUG)
-//	printf("Added!\n");
-#endif
 }
 
 Textbox* GetActivatedTextBox(){//接口 : 返回 ActivatedTextBox 的值
